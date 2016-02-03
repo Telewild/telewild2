208a28db0205f4152b25add11338bd790ad0aa58
AliKhodadad <alikhodadad44@gmail.com>
2016-02-03 16:42:46 +0330

    Update and rename plugins/tosupport.lua to poshtibani.lua

---
+++ poshtibani.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+  return "@DARK_BOYS_ADMIN"
+  end
+return {
+  description = "shows @DARK_BOYS_ADMIN", 
+  usage = "!poshtibani : @DARK_BOYS_ADMIN"",
+  patterns = {
+    "^poshtibani$",
+    "^/poshtibani$",
+    "^!poshtibani$",
+    "^>poshtibani$",
+  },
+  run = run
+}
+end
